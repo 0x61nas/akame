69768e1f93555f4ca234e25beea9c8efc30aceac
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2024-03-19 11:47:16 +0200

    refactor(bin): use not_found macro

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,4 @@
-use servi::{Response, Router, Server};
+use servi::{not_found, Response, Router, Server, StatusCode};
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -19,12 +19,11 @@ impl Router for MyRouter {
     async fn route(&self, ctx: servi::Context) -> servi::Result<Response> {
         let path = String::from_utf8(ctx.req_line.path.clone()).unwrap();
         // let _ = dbg!(ctx.into_request().await);
-        Ok(match path.as_str() {
+        match path.as_str() {
             "/" => Response::builder()
-                .body(b"Ohayou!".iter().map(|b| b.to_owned()).collect::<Vec<u8>>())?,
-            // .unwrap()
-            "/ohayou" => "おはよう!".into(),
-            _ => todo!(),
-        })
+                .body(b"Ohayou!".iter().map(|b| b.to_owned()).collect::<Vec<u8>>()),
+            "/ohayou" => Ok("おはよう!".into()),
+            _ => not_found!(),
+        }
     }
 }
