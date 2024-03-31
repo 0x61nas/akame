04a93e46761cf5aab2752f0a627273236d6954b2
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2024-03-31 13:17:34 +0200

    chore(demo): use the new api

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,47 +1,47 @@
 use std::ops::Deref;
 
-use http::header;
-use servi::{not_found, Response, Router, Server};
-use tokio::{io::AsyncReadExt, net::TcpListener};
+use servi::preload::*;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    //
     let listener = TcpListener::bind(("127.0.0.1", 6173)).await.unwrap();
 
     let mut server = Server::new(MyRouter, listener);
     println!("starting the server");
     server.start().await.unwrap();
-
-    println!("test");
 }
 
 struct MyRouter;
 
 impl Router for MyRouter {
-    async fn route(&self, ctx: servi::Context) -> servi::Result<Response> {
-        // let request = ctx.into_request().await?;
-        // let _ = dbg!(ctx.into_request().await);
-        let ctx = dbg!(&ctx);
-        match dbg!(ctx.path().chunks()).deref() {
-            // ["/"] => Response::builder()
-            //     .body(b"Ohayou!".iter().map(|b| b.to_owned()).collect::<Vec<u8>>()),
-            ["/"] => Ok(include_bytes!("../../index.html").to_vec().into()),
+    async fn route(&self, mut ctx: servi::Context) -> servi::Result<Response> {
+        let path = ctx.path();
+        match path.deref() {
+            ["/"] => Ok(include_bytes!("../../pub/index.html").to_vec().into()),
+            ["login"] | ["login.html"] => resource!("pub/login.html", mime::TEXT_HTML_UTF_8),
+            [.., "style", _] => resource!(format!("pub/{path}"), mime::TEXT_CSS_UTF_8),
             ["ohayou"] => Ok("おはよう!".into()),
-            ["echo", what] => Ok(what.as_bytes().into()),
-            // ["/", "echo"] => {
-            //     let mut request = ctx.into_request().await?;
-            //     let mut buf = Vec::with_capacity(
-            //         request
-            //             .headers
-            //             .get(header::CONTENT_LENGTH)
-            //             .map(|v| v.to_str().unwrap().parse::<usize>().unwrap())
-            //             .unwrap_or(512),
-            //     );
-            //     request.body.read_to_end(&mut buf).await?;
-            //     Ok(buf.into())
-            // }
-            _ => not_found!("OOPS, 404 -\\()/-".as_bytes().to_vec()),
+            ["echo", what] => Ok((*what).into()),
+            ["echo"] => {
+                drop(path); // yup, c
+                let mut buf = Vec::with_capacity(512);
+                ctx.request.read_body(&mut buf).await?;
+                Ok(buf.into())
+            }
+            ["static", ..] => resource!(path, mime::IMAGE_GIF),
+            ["json"] => json!({
+                "code": 200,
+                "success": true,
+                "payload": {
+                    "features": [
+                        "serde",
+                        "json"
+                    ],
+                    "homepage": null
+                }
+            }),
+            _ => not_found!("Oh, what this even mean??"),
         }
     }
 }
